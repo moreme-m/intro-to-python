3a423b1ccf24f6c5559f1a375788b11c69eb736f
moreme-m <mariamlaw2011@gmail.com>
2025-02-09 17:53:54 +0100

    intro python

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Mariam")
+print("I love to code")
+print()
+
+name = "Mariam"
+hobby = "code"
+age = 13
+colour = "purple"
+
+print("My name is ",name,"and i am ",age,".My favourite color is ",colour,".I love to ",hobby)
