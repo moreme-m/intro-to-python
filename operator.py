23ab4dd1e8abae8238a6a3861fc75a4cf8902444
moreme-m <mariamlaw2011@gmail.com>
2025-02-16 17:40:07 +0100

    operators

---
+++ operator.py
@@ -0,0 +1,45 @@
+# # type: ignore
+# age = 30
+# weight = 70.5
+# name = "Mariam"
+# is_student = False
+
+# print(type(age)) 
+# print(type(weight)) 
+# print(type(name)) 
+# print(type(is_student)) 
+
+# print("1"+"1")
+
+# 3
+# #typecasting = changing one data type to another
+
+# print(age)
+# age = float(age)
+# print(age)
+
+# print(age)
+# age = bool(age)
+# print(age)
+
+# print(1+1)
+
+# print("The addition of 1 and 5 is",1+5)
+
+# print(name)
+# name = bool(name)
+# print(name)
+
+num1 = 60
+num2 = 80
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** 2)    #square
+print(num1 ** 0.5)   #square root
+
+print(num1 == num2)    #equal to 
+print(num1 > num2)
+print(num1 < num2)
+print(num1 != num2)     #not equal to
