aec82b718484fce5b8cd7573ba290341ed535110
moreme-m <mariamlaw2011@gmail.com>
2025-02-22 18:02:26 +0100

    conditions

---
+++ l3-python conditions/python.py
@@ -0,0 +1,26 @@
+weight = 60
+height = 16.4
+BMI = weight/(height*height)
+
+if BMI <= 18.4:
+    print("You are underweight")
+
+elif BMI <=24.9:
+    print("You are healthy")   
+
+elif BMI <= 29.9:
+    print("You are overweight")  
+
+elif BMI <= 34.9:
+    print("You are severely overweight")
+
+elif BMI <= 39.9:
+    print("You are obese")      
+
+else :
+    print("You are severely obese")  
+
+import datetime
+print(datetime.datetime.now())
+import calendar
+print(calendar.calendar(3025))       
